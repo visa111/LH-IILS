5f247fb4a0c2da7093eb832a3783ab3fdc2e67ef
visa111 <visaruttor@gmail.com>
2018-07-24 16:23:23 +0700

    Update newbotpush.php

--- newbotpush.php
+++ newbotpush.php
@@ -1,5 +1,5 @@
 <?php
-   $accessToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+   $accessToken = "dqlJ7qi+56CkIeXXjTYdu+vdV2pwsjB0dY9UMQDNO7be8ymtAVJO6PUkAxBKR0hdMde88YOy+xwPVGYUz06KWEZqjSo7g0o+GFkvBeHp+nnFblLPeRgz+pxef1wfVnIKs66z7jZtQzPonUFouqQFCAdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
    $content = file_get_contents('php://input');
    $arrayJson = json_decode($content, true);
    $arrayHeader = array();
@@ -27,6 +27,21 @@
       $arrayPostData['messages'][2]['text'] = $group;
       pushMsg($arrayHeader,$arrayPostData);
    }
+#ตัวอย่าง Message Type "Text + Sticker"
+   if($message == "สวัสดี" or $message == "สวัสดีครับ"){
+      $arrayPostData['to'] = $id;
+      $arrayPostData['messages'][0]['type'] = "text";
+      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+      $arrayPostData['messages'][1]['type'] = "sticker";
+      $arrayPostData['messages'][1]['packageId'] = "2";
+      $arrayPostData['messages'][1]['stickerId'] = "34";
+      $arrayPostData['messages'][2]['type'] = "text";
+      $arrayPostData['messages'][2]['text'] = $id;
+      $arrayPostData['messages'][2]['type'] = "text";
+      $arrayPostData['messages'][2]['text'] = $group;
+      pushMsg($arrayHeader,$arrayPostData);
+   }
+
 
 if($message == "นับ 1-10"){
        for($i=1;$i<=10;$i++){
@@ -36,6 +51,7 @@ if($message == "นับ 1-10"){
           pushMsg($arrayHeader,$arrayPostData);
        }
     }
+
 
 
 
